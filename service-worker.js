0993e21e41c41e0112c5c7b2d07efb6dd5c0f085
jnyskg-ai <jny.skg@gmail.com>
2026-08-21 09:44:41 +0200

    fix: correct PWA cache cleanup

--- service-worker.js
+++ service-worker.js
@@ -25,7 +25,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME).map(key => cache.delete(key))))
+      .then(keys => Promise.all(keys.filter(key => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
